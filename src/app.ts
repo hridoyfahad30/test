34c373a3d28b951ac99e442c877e8ec288e8192f
Fahad Ibne Hossain <hridoyfahad30@gmail.com>
2024-01-24 16:18:49 +0600

    Second

---
+++ src/app.ts
@@ -0,0 +1,22 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  res.status(200).json({
+    message: `App is listening on port 5000`,
+  });
+};
+
+app.get('/', getAController);
+
+export default app;
